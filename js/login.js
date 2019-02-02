7d67c6bb179a73885e4cb9066108b398b1d288b2
Sunyoung Shin <s2017i30@e-mirim.hs.kr>
2019-02-03 02:03:45 +0900

    [WHILE] 로그인 화면 배경 이미지 페이드

---
+++ js/login.js
@@ -0,0 +1,18 @@
+
+window.onload = init;
+
+function init() {
+
+    $(document).ready(function () {
+        var images = ["image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg"];
+        $(function () {
+            
+            $.backstretch(['assets/image1.jpg', 'assets/image2.jpg', 'assets/image3.jpg'], {duration:3500, fade:750});
+            
+
+        });
+    });
+
+}
+
+
